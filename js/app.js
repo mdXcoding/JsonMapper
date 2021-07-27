b2fa4f72eabcd2d139e06d2c876a7701bf3ce518
Michele Danzè <me@micheledanze.com>
2021-07-27 16:31:51 +0200

    Removed unused files, added append/prepend function. Re-arranged the code.

--- js/app.js
+++ js/app.js
@@ -1,52 +1,68 @@
 var app = new Vue({
-  el: '#app',
-  methods:
-  {
-  	parseJson()
-  	{
-  		this.jsonError = false;
+	el: '#app',
+	methods:
+	{
+		parseJson()
+		{
+			this.jsonError = false;
 
-  		try
-  		{
-  			this.rows = JSON.parse(this.rawJson);
-  			this.generateKeys();
-  		}
-  		catch(e)
-  		{
-  			this.jsonError = true;
-  		}
-  	},
-  	generateKeys()
-  	{
-  		this.keys = [];
+			try
+			{
+				this.rows = JSON.parse(this.rawJson);
+				this.generateKeys();
+			}
+			catch(e)
+			{
+				this.jsonError = true;
+			}
+		},
+		generateKeys()
+		{
+			this.keys = [];
 
-  		if (this.rows && this.rows.length)
-  		{
-  			this.keys = Object.keys(this.rows[0]);
-  		}
-  	},
-  	generateCommands()
-  	{
-  		this.message = this.rows.map(row => '!' + row[this.selectedKey]).join(', ');
-  	}
-  },
-  watch:
-  {
-  	selectedKey()
-  	{
-  		this.generateCommands();
-  	},
-  	rawJson()
-  	{
-  		this.parseJson();
-  	}
-  },
-  data: {
-    message: "",
-    rawJson: "",
-    keys: [],
-    rows: [],
-    selectedKey: "",
-    jsonError: false,
-  }
+			if (this.rows && this.rows.length)
+			{
+				this.keys = Object.keys(this.rows[0]);
+			}
+		},
+		generateCommands()
+		{
+			this.message = this.rows.map(row => this.options.prepend + row[this.selectedKey] + this.options.append).join(", ");
+		}
+	},
+	mounted() {
+		this.parseJson();
+		this.generateCommands();
+	},
+	watch:
+	{
+		selectedKey()
+		{
+			this.generateCommands();
+		},
+		rawJson()
+		{
+			this.parseJson();
+		},
+        options:
+        {
+            handler()
+            {
+                this.generateCommands();
+            },
+            deep: true,
+        }
+	},
+	data: {
+		message: "",
+		rawJson: "[{\"key\": \"my value 1\"}, {\"key\": \"my value 2\"}]",
+		keys: [],
+		rows: [],
+		selectedKey: "key",
+		jsonError: false,
+		options: {
+			append: "",
+			prepend: "!",
+		}
+	}
 });
